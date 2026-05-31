ce160fa9db6bfa0f3f854ee48625b834d97d9269
Afanasiev <gecktor.86@mail.ru>
2026-05-31 19:20:50 +0400

    fixed src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -8,22 +8,25 @@ def get_mask_account_card(account_info: str) -> str:
     """Функция принимает строку, в которой передается информация о типе данных(счет/карта) и номере данных,
     определяет тип данных и использует фунции для маскировки номера соответствуе типу данных"""
 
+    account_name = ""
     number_info = ""
 
     # выводим номер карты в переменную
-    match = re.search(r"[0-9]+", account_info)
+    match = re.search(r"([a-zA-Zа-яА-Я\s]+)([0-9]+)", account_info)
 
     # проверка на совпадение поиска
     if match:
-        number_info = match.group()
+        account_name = match.group(1)
+        number_info = match.group(2)
 
     # определяем к какому типу данных относится номер по длине строки и возвращаем маску номера
     try:
         if len(number_info) == 16:
-            return masks.get_mask_card_number(number_info)
+            return f"{account_name}{masks.get_mask_card_number(number_info)}"
 
         elif len(number_info) == 20:
-            return masks.get_mask_account(number_info)
+            return f"{account_name}{masks.get_mask_account(number_info)}"
+
         else:
             raise FormatError()
 
