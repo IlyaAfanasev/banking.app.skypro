aa1e15c8885c0534e84b8f441e9bd415b508b9e0
Afanasiev <gecktor.86@mail.ru>
2026-06-01 19:53:17 +0400

    add def sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,16 @@
 from typing import List
 
 
-def filter_by_state(banking_operations: List[dict], state: str='EXECUTED') -> List[dict]:
+def filter_by_state(banking_operations: List[dict], state: str = 'EXECUTED') -> List[dict]:
     """Функция принимает список словарей банковских операций и опционально значение state
     по которому будет производиться фильтрация"""
 
-    return [i for i in banking_operations if i["state"]==state]
+    return [i for i in banking_operations if i["state"] == state]
 
+
+def sort_by_date(banking_operations: List[dict], rev: bool = False) -> List[dict]:
+    """Функция принимает список словарей банковских операций и опционально булево значение rev,
+     сортирует список по дате и реверс по значению rev, по умолчанию False"""
+
+    return sorted(banking_operations, key=lambda x: x["date"], reverse=rev)
 
