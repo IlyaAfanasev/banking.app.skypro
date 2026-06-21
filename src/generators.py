2139a030f30d9a06218cb37eaee4a7f8a3038b2a
Afanasiev <gecktor.86@mail.ru>
2026-06-21 21:00:55 +0400

    add tests for methods filter_by_currency, filter_by_currency, card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,11 +1,11 @@
-from typing import Iterable, Generator
+from typing import Generator, Iterable
 
 
-def filter_by_currency(transactions: list[dict], currensy_code: str) -> Iterable:
+def filter_by_currency(transactions: list[dict], currency_code: str) -> Iterable:
     """Функция принимает список словарей с транзакциями и возвращает итератор,
     который поочередно выдает транзакции, где валюта операции соответствует заданной"""
 
-    return (x for x in transactions if x["operationAmount"]["currency"]["code"] == currensy_code)
+    return (x for x in transactions if x["operationAmount"]["currency"]["code"] == currency_code)
 
 
 def transaction_descriptions(transactions: list[dict]) -> Generator:
@@ -20,9 +20,13 @@ def card_number_generator(start: int, finish: int) -> Generator:
     в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
     Генератор принимает начальное и конечное значения для генерации диапазона номеров."""
 
+    if start > finish:
+        raise ValueError
+
     for number in range(start, finish + 1):
         str_number = str(number).zfill(16)
 
+        if number > 9999_9999_9999_9999 or number < 0:
+            raise ValueError
+
         yield f"{str_number[:4]} {str_number[4:8]} {str_number[8:12]} {str_number[12:]}"
-        if number == 9999_9999_9999_9999:
-            return
---
+++ tests/test_generators.py
@@ -0,0 +1,38 @@
+import pytest
+
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from tests.conftest import card_numbers
+
+
+def test_filter_by_currency(transactions, transactions_with_currency_code_usd):
+    assert list(filter_by_currency(transactions, "USD")) == transactions_with_currency_code_usd
+
+
+def test_filter_by_currency_with_empty_list():
+    with pytest.raises(StopIteration):
+        next(filter_by_currency([], "USD"))
+
+
+def test_filter_by_currency_code_not_included(transactions):
+    with pytest.raises(StopIteration):
+        next(filter_by_currency(transactions, "CNY"))
+
+
+def test_transaction_descriptions(transactions, description_of_transactions):
+    assert list(transaction_descriptions(transactions)) == description_of_transactions
+
+
+def test_transaction_descriptions_with_empty_list(transactions):
+    with pytest.raises(StopIteration):
+        next(transaction_descriptions([]))
+
+
+def test_card_number_generator(card_numbers):
+    assert list(card_number_generator(1, 5)) == card_numbers
+
+
+def test_card_number_generator_with_wrong_parameters():
+    with pytest.raises(ValueError):
+        next(card_number_generator(-1, 6))
+        next(card_number_generator(8, 6))
+        next(card_number_generator(1_0001_1111_1111_1111, 1_0001_1111_1111_1113))
