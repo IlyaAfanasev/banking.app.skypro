5b9baedb88929490af18d8d8da45d94bd80a2174
Afanasiev <gecktor.86@mail.ru>
2026-06-22 22:33:58 +0400

    edit README.md

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,7 +1,6 @@
 import pytest
 
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
-from tests.conftest import card_numbers
 
 
 def test_filter_by_currency(transactions, transactions_with_currency_code_usd):
